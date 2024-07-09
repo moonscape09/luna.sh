da1b6f5bca0def21c89aaa9d6bf63550c56c1364
moonscape09 <munim.adil@outlook.com>
2024-07-09 19:52:49 -0400

    New and easy setup steps using Conda environments.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # luna.sh
-Welcome to `luna.sh`. A shell I built as part of my C++ journey! Stay tuned! 🌙 
+Welcome to `luna.sh` 🌙. A shell I am building as I learn C++. Stay tuned!
+
+## Prerequisites:
+- Ensure Conda is installed on your system.
+
+## Setup Instructions:
+1. Create the Conda environment from `luna-environment.yml`:
+   ```bash
+   conda env create -f luna-environment.yml
+   ```
+2. Activate the Conda environment
+  ```bash
+  conda activate luna-env
+  ```
+3. Launch `luna.sh`
+  ```bash
+  source launch.sh
+  ```
--- launch.sh
+++ launch.sh
@@ -6,9 +6,13 @@ cp -r src build/src
 cp -r tests build/tests
 cp -r include build/include
 cd build/src
-export LDFLAGS="-L/opt/homebrew/opt/readline/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/readline/include"
 g++ -std=c++20 *.cpp ../tests/test.cpp commands/*cpp -o luna.sh -lreadline $LDFLAGS $CPPFLAGS
+
+export LDFLAGS="-L$CONDA_PREFIX/lib"
+export CPPFLAGS="-I$CONDA_PREFIX/include"
+export DYLD_LIBRARY_PATH="$CONDA_PREFIX/lib:$DYLD_LIBRARY_PATH"
+
+# debug, testing and leaks mode for own use
 if [[ $1 = "--testing" ]]; then
     export "LUNA_TESTING_ON"=
     echo "Hello world\n" >> ../cat_testing.txt
@@ -16,7 +20,7 @@ if [[ $1 = "--testing" ]]; then
 else
     unset "LUNA_TESTING_ON" # unset in case testing mode was ran before
 fi
-if [[ $1 = "--debug" ]]; then
+if [[ $1 = "--debug" ]]; then # opens debugging interface
     lldb ./luna.sh
 elif [[ $1 = "--leaks" ]]; then # checking for memory leaks
     leaks -atExit -- ./luna.sh
